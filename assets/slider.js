23ab13ff632161ca9e518bd78ebb880ceeb1ef7b
marta-wijaya <78483470+marta-wijaya@users.noreply.github.com>
2022-02-09 22:11:45 +0800

    Slider

--- assets/slider.js
+++ assets/slider.js
@@ -6,33 +6,9 @@ const SLider = {
         const slider = document.querySelector('.slider-collection');
 
         if (slider) {
-            const swiper = new Swiper('.slider-collection', {
-                loop: false,
-                spaceBetween: 30,
-                resistance: true,
-                resistanceRatio: 0,
-                breakpoints: {
-                    200: {
-                        slidesPerView: 1,
-                        spaceBetween: 30
-                    },
-                    480: {
-                        width: 300,
-                        spaceBetween: 25
-                    },
-                    768: {
-                        slidesPerView: 3,
-                        spaceBetween: 30
-                    },
-                    991: {
-                        slidesPerView: 4,
-                        spaceBetween: 30
-                    },
-                },
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev'
-                }
+            var flkty = new Flickity( '.slider-collection', {
+                cellAlign: 'center',
+                contain: true
             });
         }
     }
