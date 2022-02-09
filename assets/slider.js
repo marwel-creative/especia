dd82685c85a7afe14832cafaba59c9817adfe0e4
marta-wijaya <78483470+marta-wijaya@users.noreply.github.com>
2022-02-09 21:29:50 +0800

    Slider

--- assets/slider.js
+++ assets/slider.js
@@ -1,24 +1,40 @@
 const SLider = {
     init: () => {
-        // SLider.scroll();
+        SLider.scroll();
     },
     scroll: () => {
-        // function resizeHeaderOnScroll() {
-        //     const distanceY = window.pageYOffset || document.documentElement.scrollTop,
-        //         shrinkOn = 50,
-        //         headerEl = document.querySelector('sticky-header'),
-        //         body = document.body;
-        //     if (headerEl) {
-        //         if (distanceY > shrinkOn) {
-        //             headerEl.classList.add('scrolled');
-        //             body.classList.add('header-scrolled');
-        //         } else {
-        //             headerEl.classList.remove('scrolled');
-        //             body.classList.remove('header-scrolled');
-        //         }
-        //     }
-        // }
-        // window.addEventListener('scroll', resizeHeaderOnScroll);
+        const slider = document.querySelector('.slider-post__content');
+
+        if (slider) {
+            const swiper = new Swiper('.slider-post__slider', {
+                loop: false,
+                spaceBetween: 30,
+                resistance: true,
+                resistanceRatio: 0,
+                breakpoints: {
+                    200: {
+                        slidesPerView: 1,
+                        spaceBetween: 30
+                    },
+                    480: {
+                        width: 300,
+                        spaceBetween: 25
+                    },
+                    768: {
+                        slidesPerView: 3,
+                        spaceBetween: 30
+                    },
+                    991: {
+                        slidesPerView: 4,
+                        spaceBetween: 30
+                    },
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev'
+                }
+            });
+        }
     }
 };
 
